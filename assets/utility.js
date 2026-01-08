01c43940192a68b788f9716447714d4780c44e94
MicTarHal <tarina.michal@gmail.com>
2026-01-08 22:03:05 +0100

    Reworked status-tag yet again.

--- assets/utility.js
+++ assets/utility.js
@@ -1,23 +1,25 @@
-window.updateGridStatusTag = function(status_values){
+window.updateGridStatusTag = function(){
     const now = new Date();
+
+    const entities = document.querySelector("menu.entities");
   
-    document.querySelectorAll(".entities article").forEach(article => {
-      const endTime = article.querySelector("header > .timeline > time:last-child");
+    entities.querySelectorAll(":scope article").forEach(article => {
+      const endTime = article.querySelector(":scope > header > .timeline > time:last-child");
       const date = new Date(endTime.dateTime);
       const tag = article.querySelector("header > .status-tag");
       const key = date.getTime() < now.getTime() ? "archived" : "active";
-      tag.textContent = status_values[key];
+      tag.textContent = entities.dataset[key];
       tag.classList.add(key);
     });
   }
 
-  window.updatePageStatusTag = function(status_values){
+  window.updatePageStatusTag = function(){
     const now = new Date();
 
     const attributes = document.querySelector("#project .attributes");
     const statusTag = attributes.querySelector(".status");
     const date = new Date(attributes.querySelector(".timeline time:last-child").dateTime);
     const key = date.getTime() < now.getTime() ? "archived" : "active";
-    statusTag.textContent = status_values[key];
+    statusTag.textContent = statusTag.dataset[key];
     statusTag.classList.add(key);
   }
